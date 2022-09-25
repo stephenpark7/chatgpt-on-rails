385bf826fe30e951c4162a50f7cb39b6a9155fdc
Stephen Park <spark0843@gmail.com>
2022-09-25 00:56:00 -0400

    feat: add dotenv-rails

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Dotenv::Railtie.load unless ['production', 'staging'].include?(Rails.env)
 
 module UntitledRailsProject
   class Application < Rails::Application
