fa9ad83cfc84ed7940e15ac6987d1f74df6779da
Stephen Park <stephen@customtattoodesign.ca>
2023-04-15 19:49:31 -0400

    feat: improve responses

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,17 +2,28 @@ class PublicController < ActionController::Base
   skip_before_action :verify_authenticity_token
 
   def index
-    @messages = Message.all
+    @messages = Message.order("id ASC")
     @user = User.first
     @chatgpt = User.second
   end
 
   def messages
-    message = params[:message][:content]
+    message = params[:message][:content].gsub(/^\n+/, '') # Remove leading newlines
     head 400 && return if message.nil?
     # SendMessageJob.perform_later(content)
     Message.create!(user: User.first, content: message) # User
-    response = ChatGPT.send_message(Message.pluck(:content).join(''))
+
+    prompt = 'You are ChatGPT. '
+    Message.all.each do |m|
+      if m.user == User.first
+        prompt += 'Person: '
+      else
+        prompt += ''
+      end
+      prompt += m.content + '\n'
+    end
+    prompt += 'ChatGPT: '
+    response = ChatGPT.send_message(prompt)
     Message.create!(user: User.second, content: response) # ChatGPT
     render json: { message: response }
   end
