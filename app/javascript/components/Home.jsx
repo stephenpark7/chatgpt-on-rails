b4e0edbc69d7756a10421dae9049826b035d808c
Stephen Park <stephen@customtattoodesign.ca>
2023-04-16 04:04:17 -0400

    add tailwindcss

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Home = () => {
   return (
     <div>
-      <h1>
+      <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
     </div>
